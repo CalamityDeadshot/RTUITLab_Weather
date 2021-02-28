97d8b220d7b6fe88edf2fc7b30b60c277abb5376
CalamityDeadshot <semyon.pyankov@gmail.com>
2021-02-28 22:14:41 +0300

    Fixed onecall API method declaration

--- app/src/main/java/com/calamity/weather/data/retrofit/GetDataService.kt
+++ app/src/main/java/com/calamity/weather/data/retrofit/GetDataService.kt
@@ -1,6 +1,7 @@
 package com.calamity.weather.data.retrofit
 
 import com.calamity.weather.data.api.ApiResponseWrapper
+import com.calamity.weather.data.api.CurrentWeather
 import com.calamity.weather.data.api.Weather
 import retrofit2.Call
 import retrofit2.http.GET
@@ -15,12 +16,20 @@ interface GetDataService {
                           @Query("lang") languageCode: String
     ): Call<ApiResponseWrapper>
 
+    @GET("weather")
+    fun getCurrentWeather(@Query("appid") apiKey: String,
+                          @Query("id") cityId: Int,
+                          @Query("units") units: String,
+                          @Query("lang") languageCode: String
+    ): Call<CurrentWeather>
+
     @GET("onecall")
-    fun getWeather(@Query("lat") latitude: Double,
+    fun getWeather(@Query("appid") apiKey: String,
+                   @Query("lat") latitude: Double,
                    @Query("lon") longitude: Double,
                    @Query("exclude") exclude: String,
                    @Query("units") units: String,
                    @Query("lang") languageCode: String
-    ): Weather
+    ): Call<Weather>
 
 }
